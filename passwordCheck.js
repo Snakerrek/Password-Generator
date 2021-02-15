c3d793953d544e825170dd6c34e7602320716411
Snakerrek <snakerrek@gmail.com>
2021-02-15 21:46:51 +0100

    if statement fix

--- passwordCheck.js
+++ passwordCheck.js
@@ -87,7 +87,7 @@ function updateTime(time) {
       timePeriods[i].style.display = 'none';
     }
     timePeriods[0].innerText = `Infinite time`;
-  } else if (time.seconds === 0) {
+  } else if (time.seconds < 1) {
     for (let i = 1; i < timePeriods.length; i++) {
       timePeriods[i].style.display = 'none';
     }
